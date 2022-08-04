c1ed608df81e971d71ed34d8950dd1e521f98514
intituta <intituta@tutanota.com>
2022-08-04 18:08:18 +0300

    cpp

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 17:50:04 by kferterb          #+#    #+#             */
+/*   Updated: 2022/08/04 18:04:36 by kferterb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int main(int ac, char **av) {
@@ -6,7 +18,7 @@ int main(int ac, char **av) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     } else {
         for (int i = 1; av[i]; i++) {
-            for (int j = 0; av[i][j]; j++) {  
+            for (int j = 0; av[i][j]; j++) {
                 std::cout << (char)toupper(av[i][j]);
             }
         }
