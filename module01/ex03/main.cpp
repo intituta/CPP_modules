fdbed8692fb0b3eb5a0a8a717f0001af5a0fe816
intituta <intituta@tutanota.com>
2022-08-07 17:41:16 +0300

    cpp

--- module01/ex03/main.cpp
+++ module01/ex03/main.cpp
@@ -5,21 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/07 17:13:12 by kferterb          #+#    #+#             */
-/*   Updated: 2022/08/07 17:37:23 by kferterb         ###   ########.fr       */
+/*   Created: 2022/08/07 17:39:49 by kferterb          #+#    #+#             */
+/*   Updated: 2022/08/07 17:39:50 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-int main(void) {
-	std::string string = "HI THIS IS BRAIN";
-	std::string &stringREF = string;
-	std::string *stringPTR = &string;
-	std::cout	<< "string address: " << &string << "\n"
-				<< "stringPTR address: " << stringPTR << "\n"
-				<< "stringREF address: " << &stringREF << "\n"
-				<< "value string: " << string << "\n"
-				<< "value stringPTR: " << *stringPTR << "\n"
-				<< "value stringREF: " << stringREF << "\n";
-}
