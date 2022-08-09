874ffdc127269dd69bfa03a786239139832a55f6
intituta <intituta@tutanota.com>
2022-08-09 10:37:04 +0300

    cpp

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -6,7 +6,42 @@
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 09:15:43 by kferterb          #+#    #+#             */
-/*   Updated: 2022/08/09 09:15:44 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/08/09 10:33:22 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+using std::cout;
+using std::endl;
+
+Fixed::Fixed() : value(0) {
+	cout << "Default constructor called" << endl;
+}
+
+Fixed::~Fixed() {
+	cout << "Destructor called" << endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+	cout << "Copy constructor called" << endl;
+	*this = fixed;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+	cout << "Copy assignment operator called" << endl;
+	if (this != &fixed) {
+		this->value = fixed.getRawBits();
+	}
+	return *this;
+}
+
+int Fixed::getRawBits() const {
+	cout << "getRawBits member function called" << endl;
+	return this->value;
+}
+
+void Fixed::setRawBits(const int raw) {
+	cout << "setRawBits member function called" << endl;
+	this->value = raw;
+}
--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,7 +6,28 @@
 /*   By: kferterb <kferterb@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 09:15:52 by kferterb          #+#    #+#             */
-/*   Updated: 2022/08/09 09:15:53 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/08/09 10:33:02 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+
+class Fixed {
+
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &fixed);
+	Fixed &operator = (const Fixed &fixed);
+
+	int getRawBits(void) const;
+	void setRawBits(const int raw);
+
+private:
+	static const int fractional = 8;
+	int value;
+};
+
+#endif
