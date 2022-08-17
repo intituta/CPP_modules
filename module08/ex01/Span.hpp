a7b695b5832687017f778058fcba1ce46b31b825
Katina Ferterbest <kferterb@pr-o2.kzn.21-school.ru>
2022-08-17 12:24:43 +0300

    cpp

--- module08/ex01/Span.hpp
+++ module08/ex01/Span.hpp
@@ -6,7 +6,34 @@
 /*   By: kferterb <kferterb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 17:55:56 by kferterb          #+#    #+#             */
-/*   Updated: 2022/08/16 17:55:57 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/08/17 12:24:04 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+
+class Span {
+public:
+	~Span();
+	Span(unsigned int N);
+	Span(const Span &span);
+	Span &operator = (const Span &span);
+
+	void addNumber(int i);
+	void addRange(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+	int shortestSpan();
+	int longestSpan();
+
+private:
+	Span();
+	unsigned int size;
+	std::vector<int> vector;
+};
+
+#endif
--- module08/ex01/main.cpp
+++ module08/ex01/main.cpp
@@ -6,7 +6,71 @@
 /*   By: kferterb <kferterb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 13:50:28 by kferterb          #+#    #+#             */
-/*   Updated: 2022/08/16 17:55:16 by kferterb         ###   ########.fr       */
+/*   Updated: 2022/08/17 12:20:42 by kferterb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+int main() {
+	Span sp = Span(5);
+
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	std::cout << "---" << std::endl;
+
+	Span spa(10000);
+	for (int i = 0; i < 10000; ++i) {
+		spa.addNumber(i);
+	}
+
+	std::cout << spa.shortestSpan() << std::endl;
+	std::cout << spa.longestSpan() << std::endl;
+	std::cout << "---" << std::endl;
+
+	Span span(1000);
+	std::srand(time(0));
+	for (int i = 0; i < 1000; ++i) {
+		int random = std::rand();
+		if (std::rand() % 2 == 0) {
+			random = -random;
+		}
+		span.addNumber(random);
+	}
+
+	std::cout << span.shortestSpan() << std::endl;
+	std::cout << span.longestSpan() << std::endl;
+	std::cout << "---" << std::endl;
+
+	std::vector<int> vector;
+	for (int i = 0; i < 10; ++i) {
+		vector.push_back(i);
+	}
+
+	Span range(15);
+	range.addRange(vector.begin(), vector.end());
+
+	try {
+		range.addRange(vector.begin(), vector.end());
+	} catch (std::exception &exception) {
+		std::cout << exception.what() << std::endl;
+	}
+
+	vector.clear();
+	for (int i = 10; i < 15; ++i) {
+		vector.push_back(i);
+	}
+
+	range.addRange(vector.begin(), vector.end());
+
+	std::cout << range.shortestSpan() << std::endl;
+	std::cout << range.longestSpan() << std::endl;
+
+	return 0;
+}
